aefc175254893d9cbb9823b24bfe362d4e28a929
JOELTORCATO <a12022@ae4outubro.pt>
2024-03-09 09:17:16 +0000

    new commit

--- modules/express.js
+++ modules/express.js
@@ -5,36 +5,64 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/users', async (req, res) => {
-  try {
-    const users = await UserModel.find({});
+app.use((req, res, next) => {
+	console.log(`Request Type: ${req.method}`);
+	console.log(`Content Type: ${req.headers['content-type']}`);
+	console.log(`Data: ${new Date()}`);
+	next();
+});
 
-    res.status(200).json(users);
-  } catch (error) {
-    return res.status(500).send(error.message);
-  }
+app.get('/users', async (req, res) => {
+	try {
+		const users = await UserModel.find({});
+
+		res.status(200).json(users);
+	} catch (error) {
+		return res.status(500).send(error.message);
+	}
 });
 
 app.get('/users/:id', async (req, res) => {
-  try {
-    const id = req.params.id;
+	try {
+		const id = req.params.id;
 
-    const user = await UserModel.findById(id);
+		const user = await UserModel.findById(id);
 
-    return res.status(200).json(user);
-  } catch (error) {
-    return res.status(500).send(error.message);
-  }
+		return res.status(200).json(user);
+	} catch (error) {
+		return res.status(500).send(error.message);
+	}
 });
 
 app.post('/users', async (req, res) => {
-  try {
-    const user = await UserModel.create(req.body);
+	try {
+		const user = await UserModel.create(req.body);
 
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+		res.status(201).json(user);
+	} catch (error) {
+		res.status(500).send(error.message);
+	}
+});
+
+app.patch('/users/:id', async (req, res) => {
+	try {
+		const id = req.params.id;
+		const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+		res.status(200).json(user);
+	} catch (error) {
+		res.status(500).send(error.message);
+	}
+});
+
+app.delete('/users/:id', async (req, res) => {
+	try {
+		const id = req.params.id;
+		const user = await UserModel.findByIdAndDelete(id);
+
+		res.status(200).json(user);
+	} catch (error) {
+		res.status(500).send(error.message);
+	}
 });
 
 const port = 8080;
